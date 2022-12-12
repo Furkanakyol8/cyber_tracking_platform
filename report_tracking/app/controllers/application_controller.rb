cec373e1dd0838a7253871b092effe3bd9d152df
Vesile Yavuz <vesileyavuz2@gmail.com>
2022-12-12 15:35:00 +0300

    tema giydirildi

--- report_tracking/app/controllers/application_controller.rb
+++ report_tracking/app/controllers/application_controller.rb
@@ -8,4 +8,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone, :username, :company_info, :role])
   end
+
 end
