5695a9552943046ed0ad65100dbf7f8d4f65d9b7
Furkan_ <furkan.akyol28@hotmail.com>
2022-12-16 09:21:35 +0300

    pages and controllers created, customer authorization completedd

--- report_tracking/app/controllers/application_controller.rb
+++ report_tracking/app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def authorization_check
+    # If either one of them is true this filter won’t do anything, allowing the requested user registration form to be rendered
+    if current_user.role != "admin" # checking if there are either zero registered users OR if there is a user already logged in
+      redirect_to root_path # if neither are true, then redirect to root_path and return false
+      return false
+    end
+  end
 
   protected
 
