f2c545654e4f119edd3be252659da021ffd2f2bc
vesileyavuz <vesileyavuz2@gmail.com>
2023-01-25 17:49:26 +0300

    Breadcrumb eklendi

--- report_tracking/app/controllers/application_controller.rb
+++ report_tracking/app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  helper :breadcrumb
+  
   include Pundit
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -23,5 +25,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone, :username, :company_info, :role])
   end
+
 
 end
---
+++ report_tracking/app/helpers/breadcrumb_helper.rb
@@ -0,0 +1,35 @@
+module BreadcrumbHelper
+  def breadcrumb(name)
+    case name
+    when :home
+      content_tag(:li, class: "breadcrumb-item") do
+        link_to "Ana Sayfa", root_path
+      end
+    when :profiles
+      content_tag(:li, class: "breadcrumb-item") do
+        link_to "Profil", profile_path
+      end
+    when :customers
+      content_tag(:li, class: "breadcrumb-item") do
+        link_to "Müşteriler", customers_path
+      end
+    when :reports
+      content_tag(:li, class: "breadcrumb-item") do
+        link_to "Raporlar", reports_path
+      end
+    when :services
+      content_tag(:li, class: "breadcrumb-item") do
+        link_to "Hizmetler", services_path
+      end
+    when :about
+      content_tag(:li, class: "breadcrumb-item") do
+        link_to "Hakkımızda", about_path
+      end
+    when :contact
+      content_tag(:li, class: "breadcrumb-item") do
+        link_to "İletişim", contact_path
+        
+      end
+    end
+  end
+end
